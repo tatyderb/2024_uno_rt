ad644694badda1760029e2ed9cbd831cb15dd0f9
tatyderb <tatyderb@gmail.com>
2024-06-25 17:39:06 +0300

    Card в строку и из строки.

--- scr/card.py
+++ scr/card.py
@@ -1,8 +1,22 @@
 class Card:
     COLORS = ('red', 'green', 'blue', 'yellow')
+    DCOLORS = {color[0]: color for color in COLORS}
     NUMBERS = list(range(10))
 
     def __init__(self, color: str, number: int):
         self.color = color
         self.number = number
-        
+
+    def __repr__(self):
+        letter = self.color[0]
+        return f'{letter}{self.number}'
+
+    @classmethod
+    def create(cls, text: str):
+        """Создает карту из строки вида 'y8' и возвращает её."""
+        # text = 'y8'
+        letter = text[0]   # 'y'
+        number = text[1]   # '8'
+        color = cls.DCOLORS[letter]
+        card = Card(color, int(number))
+        return card
--- test/test_card.py
+++ test/test_card.py
@@ -6,3 +6,12 @@ def test_init():
     assert c.color == 'yellow'
     assert c.number == 8
 
+def test_print():
+    c = Card('yellow', 8)
+    assert str(c) == 'y8'
+
+def test_create_from_str():
+    c = Card.create('b5')
+    assert c.color == 'blue'
+    assert c.number == 5
+
