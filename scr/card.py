da860af9ea53d8259406e8db15c48bb228ade3ba
tatyderb <tatyderb@gmail.com>
2024-06-25 17:47:44 +0300

    Card можно ли играть одну карту на другую карту.

--- scr/card.py
+++ scr/card.py
@@ -20,3 +20,12 @@ class Card:
         color = cls.DCOLORS[letter]
         card = Card(color, int(number))
         return card
+
+    def playable(self, other) -> bool:
+        """Можно ли играть карту self на карту other."""
+        return self.color == other.color or self.number == other.number
+
+    @staticmethod
+    def all_cards():
+        """Возвращает все карты."""
+        pass
--- test/test_card.py
+++ test/test_card.py
@@ -15,3 +15,15 @@ def test_create_from_str():
     assert c.color == 'blue'
     assert c.number == 5
 
+def test_playable():
+    c1 = Card('yellow', 8)
+    c2 = Card('yellow', 1)
+    c3 = Card('blue', 8)
+    c4 = Card('yellow', 8)
+    c5 = Card('green', 4)
+
+    assert c1.playable(c2)
+    assert c1.playable(c3)
+    assert c1.playable(c4)
+    assert not c1.playable(c5)
+
